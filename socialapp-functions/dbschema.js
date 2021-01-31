b3c9f5f700bb04d43e8c527d00923f84a616a17d
Abishek C <abishek.c101@gmail.com>
2021-01-31 13:51:22 +0530

    done

---
+++ socialapp-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+	screams: [
+		{
+			userHandle: "user",
+			body: "this is the body of the scream",
+			createdAt: "2020-12-06T14:39:00.033Z",
+			likeCount: 5,
+			commentCount: 2,
+		},
+	],
+};
---
+++ socialapp-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	apiKey: "AIzaSyCdWW-KZopWJIjyeJg6GwdiZuHNzJO3XH0",
+	authDomain: "socialapp-32616.firebaseapp.com",
+	databaseURL: "https://socialapp-32616.firebaseio.com",
+	projectId: "socialapp-32616",
+	storageBucket: "socialapp-32616.appspot.com",
+	messagingSenderId: "458362802927",
+	appId: "1:458362802927:web:1bbc29f4d48f886009011a",
+	measurementId: "G-P6GH228VS9",
+};
